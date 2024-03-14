003bb7ea700b1b24be638e38d04f2e19b5046f3f
CodeNinja srls <67374481+codeninja-it@users.noreply.github.com>
2024-03-14 13:50:46 +0100

    Add files via upload
    versione aggiornata della normalizzazione e sanificazione tramite regex

--- UF8/Zanichelli/normalizza.php
+++ UF8/Zanichelli/normalizza.php
@@ -3,24 +3,37 @@
 	$zanichelli = mb_convert_encoding($zanichelli, "ASCII");
 	
 	$libri = [];
+	$autori = [];
 	
 	$re = '/(?<isbn>\d{13})\t(?<autori>[^\t]*)?\t(?<titolo>[^\t]*)?\t(?<sottotitolo>[^\t]*)?\t(?<edizione>[^\t]*)?\t(?<descrizione>[^\t]*)?\t(?<prezzo>\d*)?\t(?<pagine>\d*)?\t"(?<note>.*)(?<base>\d{3})x(?<altezza>\d{3}), (?<anno>\d*)"/m';
 
 	preg_match_all($re, $zanichelli, $trovati, PREG_SET_ORDER, 0);
 
-	for($i = 0; $i < sizeof($trovi); $i++){
+	for($i = 0; $i < sizeof($trovati); $i++){
 		$trovato = $trovati[$i];
 		
 		$libro = [];
-			$libro["isbn"] = $trovato["isbn"];
-			$libro["titolo"] = $trovato["titolo"];
-			$libro["sottotitolo"] = $trovato["sottotitolo"];
-			$libro["prezzo"] = $trovato["prezzo"] / 100;
-			$libro["pagine"] = $trovato["pagine"];
-			$libro["base"] = $trovato["base"];
-			$libro["altezza"] = $trovato["altezza"];
-			
+			$libro["isbn"] = intval($trovato["isbn"]);
+			$libro["titolo"] = str_replace("\"", "", $trovato["titolo"]);
+			$libro["sottotitolo"] = str_replace("\"", "", $trovato["sottotitolo"]);
+			$libro["prezzo"] = intval($trovato["prezzo"]) / 100;
+			$libro["pagine"] = intval($trovato["pagine"]);
+			$libro["base"] = intval($trovato["base"]);
+			$libro["altezza"] = intval($trovato["altezza"]);
+			$libro["anno"] = intval($trovato["anno"]);
+			$libro["idAutori"] = [];
+		
+		$scrittori = explode("-", $trovato["autori"]);
+		for($j=0; $j < sizeof($scrittori); $j++){
+			$autore = strtoupper(trim(str_replace("\"", "", $scrittori[$j])));
+			if(!isset($autori[$autore])){
+				$autori[$autore] = sizeof($autori) + 1;
+			}
+			$libro["idAutori"][] = $autori[$autore];
+		}
+		
 		$libri[] = $libro;
 	}
 	
 	file_put_contents("dati/libri.json", json_encode($libri, JSON_PRETTY_PRINT) );
+	file_put_contents("dati/autori.json", json_encode($autori, JSON_PRETTY_PRINT) );
